9db422dafd3335f0162f4adff5fb7e393bb1e76a
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-07-19 22:17:43 -0500

    update

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Button,Image ,SafeAreaView ,ScrollView,Platform} from 'react-native';
+import {pictures} from './data';
+
+const  Card  = ({name, decription, image, size}) =>{
+ return (
+    <View style = {{
+      backgroundColor: 'grey',
+      padding:15,
+      borderRadius: 40,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      margin:5,
+      
+      
+      }}>
+      <Text>{name}</Text>
+      <Image 
+      style ={{width:360,height:420,borderRadius: 40,}}
+      source={{uri:image}}
+      />
+    </View>
+  )
+}
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Open new  app!</Text>
+      <ScrollView style={{marginTop:Platform.OS === 'android'&& 30}}>
+      {pictures.map((item,i)=>{
+        return (
+          <View>
+          <Card key={i}
+       {...item}
+      />
+     </View>
+        )
+      })}
+      
+      </ScrollView>
+     
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
 });
---
+++ data.js
@@ -0,0 +1,11 @@
+ export const pictures = [
+    {name:"pepe",description:"This the best around",
+    image:'https://images.unsplash.com/photo-1568572933382-74d440642117?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZG9nc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60'
+},{
+    name:"pepe1",
+    description:"This the best around",
+    image:'https://images.unsplash.com/photo-1598133894008-61f7fdb8cc3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=388&q=80'
+},{name:"pepe2",
+description:"This the best around",
+image:'https://images.unsplash.com/photo-1558929996-da64ba858215?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZG9nc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60'
+}];
